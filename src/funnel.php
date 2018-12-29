a30173a1a85e559d4d69388368206958775560e1
Carlo Manfredi <carlomethod@gmail.com>
2018-12-29 13:36:51 +1000

    Register a funnel taxonomy

--- src/funnel.php
+++ src/funnel.php
@@ -7,5 +7,21 @@ class Funnel
 {
 	public function __construct()
 	{
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
+	}
+
+	public function register_taxonomies()
+	{
+		register_taxonomy( 'funnel', array( 'wp_block', 'page' ), array(
+			'hierarchical' => true,
+			'labels'       => array(
+				'name'          => __( 'Funnels', 'wpfunnel' ),
+				'singular_name' => __( 'Funnel', 'wpfunnel' ),
+			),
+			'rewrite'        => array(
+				'slug'         => 'funnel',
+				'hierarchical' => true
+			)
+		) );
 	}
 }

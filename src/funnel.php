d786fba1a0d5ff074a37bf9c50cc550096e53597
Carlo Manfredi <carlomethod@gmail.com>
2018-12-29 13:44:23 +1000

    Add funnel and funnel_int post type

--- src/funnel.php
+++ src/funnel.php
@@ -23,5 +23,73 @@ class Funnel
 				'hierarchical' => true
 			)
 		) );
+
+		/**
+		 * Post Type: Funnel Interiors.
+		 * Temporary until funnel infrastructure is ready
+		 */
+
+		$labels = array(
+			"name" => __( "Interiors", "wpfunnel" ),
+			"singular_name" => __( "Interior", "wpfunnel" ),
+		);
+
+		$args = array(
+			"label" => __( "Interiors", "wpfunnel" ),
+			"labels" => $labels,
+			"description" => "",
+			"public" => true,
+			"publicly_queryable" => true,
+			"show_ui" => true,
+			"show_in_rest" => true,
+			"rest_base" => "",
+			"has_archive" => false,
+			"show_in_menu" => true,
+			"show_in_nav_menus" => true,
+			"exclude_from_search" => false,
+			"capability_type" => "post",
+			"map_meta_cap" => true,
+			"hierarchical" => true,
+			"rewrite" => array( "slug" => "funnel_int", "with_front" => false ),
+			"query_var" => true,
+			"supports" => array( "title", "editor", "thumbnail", "comments", "revisions", "author", "page-attributes" ),
+		);
+
+		register_post_type( "funnel_int", $args );
+
+		/**
+		 * Post Type: Funnel Exteriors.
+		 * Temporary until funnel infrastructure is ready
+		 */
+
+		$labels = array(
+			"name" => __( "Funnels", "wpfunnel" ),
+			"singular_name" => __( "Funnel", "wpfunnel" ),
+		);
+
+		$args = array(
+			"label" => __( "Funnels", "wpfunnel" ),
+			"labels" => $labels,
+			"description" => "",
+			"public" => true,
+			"publicly_queryable" => true,
+			"show_ui" => true,
+			"show_in_rest" => true,
+			"rest_base" => "",
+			"has_archive" => false,
+			"show_in_menu" => true,
+			"show_in_nav_menus" => true,
+			"exclude_from_search" => false,
+			"capability_type" => "post",
+			"map_meta_cap" => true,
+			"hierarchical" => true,
+			"rewrite" => array( "slug" => "funnel", "with_front" => false ),
+			"query_var" => true,
+			"supports" => array( "title", "editor", "thumbnail", "comments", "revisions", "author", "page-attributes" ),
+		);
+
+		register_post_type( "funnel", $args );
+
+
 	}
 }

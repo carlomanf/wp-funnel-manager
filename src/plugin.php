adbf852394fbcfbc66f63ca6039901a01f1e2754
Carlo Manfredi <carlomethod@gmail.com>
2018-12-29 13:51:47 +1000

    View and edit funnel interiors

--- src/plugin.php
+++ src/plugin.php
@@ -28,6 +28,8 @@ class WP_Funnel_Manager
 	{
 		// Load functions
 		add_filter( 'quick_edit_dropdown_pages_args', array( $this, 'funnel_post_parent' ) );
+		add_filter( 'page_row_actions', array( $this, 'funnel_interior_edit' ), 10, 2 );
+		add_action( 'init', array( $this, 'post_parent_query_var' ) );
 
 		// Load a funnel
 		$funnel = new Funnel();
@@ -47,6 +49,36 @@ class WP_Funnel_Manager
 		$args[ 'post_type' ] = 'funnel';
 		return $args;
 	}
+
+	/**
+	 * Add a link to view and edit funnel interiors
+	 *
+	 * @since 1.0.3
+	 */
+	public function funnel_interior_edit( $actions, $post ) {
+		if ( 'funnel' != $post->post_type )
+			return $actions;
+
+		$url = admin_url( 'edit.php?post_type=funnel_int&post_parent=' . $post->ID );
+		$actions['edit_interiors'] = '<a href="' . esc_url( $url ) . '">' . __( 'Edit Interiors', 'wpfunnel' ) . '</a>';
+
+		return $actions;
+	}
+
+	/**
+	 * Allow post parent as query var
+	 * This is needed to view funnel interiors
+	 *
+	 * @since 1.0.3
+	 */
+	public function post_parent_query_var()
+	{
+		if ( !is_admin() )
+			return;
+
+		$GLOBALS['wp']->add_query_var( 'post_parent' );
+	}
+
 
 	/**
 	 * Load the plugin text domain.

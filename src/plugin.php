carlomanf/wp-funnel-manager

commit 9e3daf21c59b51d3a10fc004c474678be1ec9c32

Check valid post id

src/plugin.php
@@ -167,8 +167,7 @@ class WP_Funnel_Manager
 
 	public function trash_exterior_promote_interior( $post_id )
 	{
-		$exterior = get_post( $post_id );
-		if ( 'funnel' != $exterior->post_type )
+		if ( $exterior = get_post( $post_id ) && 'funnel' != $exterior->post_type )
 			return;
 
 		$interiors = get_posts( 'orderby=menu_order&order=ASC&post_type=funnel_int&post_parent=' . $post_id );

commit fc7c4c9bc3748d511c498cdeea59e052ebb71479

Solve #6 again (9e3daf2 broke the solution)

src/plugin.php
@@ -167,7 +167,7 @@ class WP_Funnel_Manager
 
 	public function trash_exterior_promote_interior( $post_id )
 	{
-		if ( $exterior = get_post( $post_id ) && 'funnel' != $exterior->post_type )
+		if ( !( $exterior = get_post( $post_id ) ) || 'funnel' != $exterior->post_type )
 			return;
 
 		$interiors = get_posts( 'orderby=menu_order&order=ASC&post_type=funnel_int&post_parent=' . $post_id );